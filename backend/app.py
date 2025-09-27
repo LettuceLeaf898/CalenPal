fbf524afa2bbca5bbcbf851bfef40c400ab2e312
KahlenHernani <kahlenhernani@gmail.com>
2025-09-27 09:06:22 -0400

    testing

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from Client_agent.agent import root_agent
+from Client_agent.agent import get_agent_response
 
 app = Flask(__name__, template_folder="../frontend")
 CORS(app)
@@ -17,10 +20,14 @@ def get_items():
 
 @app.route("/ask-agent", methods=["POST"])
 def ask_agent():
-    data = request.get_json()
-    Fdata = data.get("input")
-    agent_response=my_agent.run(Fdata)
-    return jsonify(agent_response), 201
+    #data = request.get_json()
+    #prompt = data.get("input")
+
+    #response_json = get_agent_response(prompt)
+   # return jsonify(response_json), 201
+   return{"testing": "testing"}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+    
